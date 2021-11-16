3dfb68438f9f7723ea07f51d18f4a918ee5549b8
Jakerez7 <>
2021-11-16 10:35:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#### Describe each datatype below:(4 marks)
+
+## 4         = 
+## 5.7       =
+## True      =
+## Good Luck =
+
+#### Which datatype would be useful for storing someone's height? (1 mark)
+## Answer
+
+#### Which datatype would be useful for storing someone's hair colour?(1 mark)
+## Answer
+
+####Create a variable tha will store the users name.(1 mark)
+
+####Create a print statement that will print the first 4 characters of the person's name.(3 marks)
+
+####Convert the user's name to all uppercase characters and print the result
+
+####Find out how many times the letter A occurs in the user's name
+
+#### Create a conditional statement to ask a user to enter their age. If they are older than 18 they receive a message saying they can enter the competition, if they are under 18, they receive a message saying they cannot enter the competition.
+
